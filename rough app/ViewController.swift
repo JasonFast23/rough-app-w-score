146087848b037c5b3bbec7b2c78e7c612fc6922b
Jason Park <jasonfast23@gmail.com>
2023-01-03 15:29:36 -0800

    Initial Commit

---
+++ rough app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rough app
+//
+//  Created by Jason Park on 1/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
